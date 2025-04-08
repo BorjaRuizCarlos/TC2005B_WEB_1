42cd32de0481b163abc5ad08f621c795d65599e0
BorjaRuizCarlos <A01231557@tec.mx>
2025-04-07 21:46:20 -0600

    Segundo avance general

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hello World");
+//Aqui podemos usar comandos como lo serian alert, confirm, prompt, typeof, etc...
+
+const colores = ["azul","rojo","verde"];
+console.log(colores[0]);
+colores[2] = 22;
+
+const alumnos = {name:"Luis"};
+alumnos.edad = 20;
+console.log(alumnos.edad);
+
+//alumnos = [];
+
+const name = "Carlos";
+const saludo = (name)=>{console.log("Hola " + name)};
+
+
+
+function msj()
+    {console.log("Hola ")};
+
+// msj(name)
+//    {console.log("Hola " + name)};
+
+msg();
+//msg("j");
